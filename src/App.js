f0a84459075ce960b7493c13b9cfa71fcf59de1d
Olivia Lewis <ollybaby12@gmail.com>
2021-04-16 23:17:15 -0400

    refactor with deployed api/db

--- src/App.js
+++ src/App.js
@@ -5,18 +5,27 @@ import Home from './Home/Home';
 import Footer from './Footer/Footer';
 import Results from './Results/Results';
 import InterviewContext from './InterviewContext';
-import store from './dummy-store';
+// import store from './dummy-store';
+import config from './config';
 import './App.css';
 import QuestionPage from './QuestionPage/QuestionPage';
 
 class App extends Component {
   state = {
+    allQuestions: [],
     questions: [],
     responses: [],
     numberOfQuestions: 5,
     currentQuestion: 0,
     error: null,
   };
+
+  setAllQuestions = allQuestions => {
+    this.setState({
+      allQuestions,
+      error: null,
+    })
+  }
 
   directToResults = () => {
     const { history } = this.props;
@@ -33,7 +42,23 @@ class App extends Component {
     this.setState({
       numberOfQuestions: quantity
     })
-    
+
+  }
+
+  getRandomQuestions = () => {
+    // console.log(this.state.allQuestions)
+    let randomQuestions = [];
+    let questions = this.state.allQuestions;
+    console.log(questions)
+    let numberOfQuestions = this.state.numberOfQuestions;
+    while (questions.length !== 0) {
+      let randomIndex = Math.floor(Math.random() * questions.length);
+      randomQuestions.push(questions[randomIndex]);
+      questions.splice(randomIndex, 1);
+    }
+    this.setState({
+      questions: randomQuestions.slice(0, numberOfQuestions)
+    })
   }
 
   handleNextQuestion = () => {
@@ -48,20 +73,68 @@ class App extends Component {
   }
 
   componentDidMount() {
-    let randomQuestions = [];
-    let questions = store.questions;
-    let numberOfQuestions = this.state.numberOfQuestions;
-    while(questions.length !== 0) {
-      let randomIndex = Math.floor(Math.random() * questions.length);
-      randomQuestions.push(questions[randomIndex]);
-      questions.splice(randomIndex, 1);
-    }
-    this.setState({
-      questions: randomQuestions.slice(0, numberOfQuestions)
+    fetch(config.API_ENDPOINT, {
+      method: 'GET',
+      headers: {
+        'content-type': 'application/json'
+      }
     })
+      .then(res => {
+        if (!res.ok) {
+          throw new Error(res.status)
+        }
+        return res.json()
+      })
+      .then(this.setAllQuestions)
+      .catch(error => this.setState({ error }))
+    // let randomQuestions = [];
+    // let questions = this.state.allQuestions;
+    // console.log(questions)
+    // let numberOfQuestions = this.state.numberOfQuestions;
+    // while (questions.length !== 0) {
+    //   let randomIndex = Math.floor(Math.random() * questions.length);
+    //   randomQuestions.push(questions[randomIndex]);
+    //   questions.splice(randomIndex, 1);
+    // }
+    // this.setState({
+    //   questions: randomQuestions.slice(0, numberOfQuestions)
+    // })
+    // getRandomQuestions = () => {
+    //   console.log(this.state.allQuestions)
+    //   let randomQuestions = [];
+    //   let questions = this.state.allQuestions;
+    //   console.log(questions)
+    //   let numberOfQuestions = this.state.numberOfQuestions;
+    //   while (questions.length !== 0) {
+    //     let randomIndex = Math.floor(Math.random() * questions.length);
+    //     randomQuestions.push(questions[randomIndex]);
+    //     questions.splice(randomIndex, 1);
+    //   }
+    //   this.setState({
+    //     questions: randomQuestions.slice(0, numberOfQuestions)
+    //   })
+    // }
+    // this.getRandomQuestions()
   }
 
+  // renderRandomQuestions(){
+  //   const {allQuestions} = this.state;
+  //     let randomQuestions = [];
+  //     let questions = this.state.allQuestions;
+  //     console.log(questions)
+  //     let numberOfQuestions = this.state.numberOfQuestions;
+  //     while (questions.length !== 0) {
+  //       let randomIndex = Math.floor(Math.random() * questions.length);
+  //       randomQuestions.push(questions[randomIndex]);
+  //       questions.splice(randomIndex, 1);
+  //     }
+  //     this.setState({
+  //       questions: randomQuestions.slice(0, numberOfQuestions)
+  //     })
+  // }
+
   render() {
+    console.log(this.state.allQuestions)
     const contextValue = {
       questions: this.state.questions,
       responses: this.state.responses,
@@ -71,6 +144,7 @@ class App extends Component {
       addResponse: this.handleAddResponse,
       nextQuestion: this.handleNextQuestion,
     }
+    
 
     return (
       <InterviewContext.Provider value={contextValue}>
---
+++ src/config.js
@@ -0,0 +1,3 @@
+export default {
+    API_ENDPOINT: `https://obscure-garden-90214.herokuapp.com/api/questions`
+}
